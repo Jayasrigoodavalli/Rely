67aecbaa61951c8571565188b5f0fed4bb94520c
Jayasrigoodavalli <50614683+Jayasrigoodavalli@users.noreply.github.com>
2019-05-14 17:38:40 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 14-May-19 2:29:54 PM
+ * Author : JAYASREE
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
